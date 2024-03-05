12c9d8b87d9a5a4b19c0686b5f017161d3916ba0
Luciano Mendonca <luciano.s.mend@gmail.com>
2024-03-05 20:34:34 -0300

    Validações em XML

---
+++ src/main/java/br/luciano/rest/UserXMLTest.java
@@ -0,0 +1,54 @@
+package br.luciano.rest;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.*;
+
+import org.junit.Test;
+
+public class UserXMLTest {
+	
+	@Test
+	public void devoTrabalharComXML() {
+		given()
+		.when()
+			.get("http://restapi.wcaquino.me/usersXML/3")
+		.then()
+			.statusCode(200)
+			
+			.rootPath("user")
+			.body("name", is("Ana Julia"))
+			.body("@id", is("3"))
+			
+			.rootPath("user.filhos")
+			.body("name.size()", is(2))
+			
+			.detachRootPath("filhos")
+			.body("filhos.name[0]", is("Zezinho"))
+			.body("filhos.name[1]", is("Luizinho"))
+			
+			.appendRootPath("filhos")
+			.body("name", hasItem("Luizinho"))
+			.body("name", hasItems("Luizinho","Zezinho"))
+		;
+	}
+	
+	@Test
+	public void devoFazerPesquisasAvancadasComXML() {
+		given()
+		.when()
+			.get("http://restapi.wcaquino.me/usersXML")
+		.then()
+			.statusCode(200)
+			.body("users.user.size()", is(3))
+			.body("users.user.findAll{it.age.toInteger() <= 25}.size()", is(2))
+			.body("users.user.@id", hasItems("1","2","3"))
+			.body("users.user.find{it.age == 25}.name", is("Maria Joaquina"))
+			.body("users.user.findAll{it.name.toString().contains('n')}.name", hasItems("Maria Joaquina","Ana Julia"))
+			.body("users.user.salary.find{it != null}.toDouble()", is(1234.5678))
+			.body("users.user.age.collect{it.toInteger() * 2}", hasItems(60,50,40))
+			.body("users.user.name.findAll{it.toString().startsWith('Maria')}.collect{it.toString().toUpperCase()}", is("MARIA JOAQUINA"))
+			
+		;
+	}
+
+}
