c78e065e7a7f2b9870b5ecff94db85caa134c2ba
Luciano Mendonca <luciano.s.mend@gmail.com>
2024-03-12 21:56:39 -0300

    URL parametrizável

--- src/main/java/br/luciano/rest/VerbosTest.java
+++ src/main/java/br/luciano/rest/VerbosTest.java
@@ -57,4 +57,42 @@ public class VerbosTest {
 			.body("salary", is(1234.5678f))
 		;
 	}
+	
+	@Test
+	public void devoCustomizarUrl() {
+		given()
+			.log().all()
+			.contentType("application/json")
+			.body("{ \"name\": \"Usuário alterado\",\"age\": 80 }")
+		.when()
+			.put("http://restapi.wcaquino.me/{entidade}/{userId}", "users", "1")
+		.then()
+			.log().all()
+			.statusCode(200)
+			.body("id", is(1))
+			.body("name", is("Usuário alterado"))
+			.body("age", is(80))
+			.body("salary", is(1234.5678f))
+		;
+	}
+	
+	@Test
+	public void devoCustomizarUrlParte2() {
+		given()
+			.log().all()
+			.contentType("application/json")
+			.body("{ \"name\": \"Usuário alterado\",\"age\": 80 }")
+			.pathParam("entidade", "users")
+			.pathParam("userId", 1)
+		.when()
+			.put("http://restapi.wcaquino.me/{entidade}/{userId}")
+		.then()
+			.log().all()
+			.statusCode(200)
+			.body("id", is(1))
+			.body("name", is("Usuário alterado"))
+			.body("age", is(80))
+			.body("salary", is(1234.5678f))
+		;
+	}
 }
