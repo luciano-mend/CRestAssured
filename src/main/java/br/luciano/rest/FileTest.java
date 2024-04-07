9ffae1e4167edc631f89493517f150b98bd29ad7
Luciano Mendonca <luciano.s.mend@gmail.com>
2024-04-07 13:44:51 -0300

    Upload de arquivo

---
+++ src/main/java/br/luciano/rest/FileTest.java
@@ -0,0 +1,39 @@
+package br.luciano.rest;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.is;
+
+import java.io.File;
+
+import org.junit.Test;
+
+public class FileTest {
+	
+	@Test
+	public void deveObrigarEnviarArquivo() {
+		given()
+			.log().all()
+		.when()
+			.post("http://restapi.wcaquino.me/upload")
+		.then()
+			.log().all()
+			.statusCode(404) //deveria ser 400
+			.body("error", is("Arquivo não enviado"))
+		;
+	}
+	
+	@Test
+	public void deveFazerUploadArquivo() {
+		given()
+			.log().all()
+			.multiPart("arquivo", new File("src/main/resources/arquivo.pdf"))
+		.when()
+			.post("http://restapi.wcaquino.me/upload")
+		.then()
+			.log().all()
+			.statusCode(200)
+			.body("name", is("arquivo.pdf"))
+		;
+	}
+
+}
