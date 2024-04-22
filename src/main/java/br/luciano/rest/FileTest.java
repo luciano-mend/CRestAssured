0803464e3b15b1ba918e0b52162256dab98e4dae
Luciano Mendonca <luciano.s.mend@gmail.com>
2024-04-22 20:39:56 -0300

    Download de arquivos

--- src/main/java/br/luciano/rest/FileTest.java
+++ src/main/java/br/luciano/rest/FileTest.java
@@ -1,10 +1,15 @@
 package br.luciano.rest;
 
 import static io.restassured.RestAssured.given;
-import static org.hamcrest.Matchers.*;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.lessThan;
 
 import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
 
+import org.hamcrest.MatcherAssert;
 import org.junit.Test;
 
 public class FileTest {
@@ -12,11 +17,11 @@ public class FileTest {
 	@Test
 	public void deveObrigarEnviarArquivo() {
 		given()
-			.log().all()
+			.log().ifValidationFails()
 		.when()
 			.post("http://restapi.wcaquino.me/upload")
 		.then()
-			.log().all()
+			.log().ifValidationFails()
 			.statusCode(404) //deveria ser 400
 			.body("error", is("Arquivo não enviado"))
 		;
@@ -25,12 +30,12 @@ public class FileTest {
 	@Test
 	public void deveFazerUploadArquivo() {
 		given()
-			.log().all()
+			.log().ifValidationFails()
 			.multiPart("arquivo", new File("src/main/resources/arquivo.pdf"))
 		.when()
 			.post("http://restapi.wcaquino.me/upload")
 		.then()
-			.log().all()
+			.log().ifValidationFails()
 			.statusCode(200)
 			.body("name", is("arquivo.pdf"))
 		;
@@ -39,17 +44,35 @@ public class FileTest {
 	@Test
 	public void naoDeveFazerUploadArquivoGrande() {
 		given()
-			.log().all()
+			.log().ifValidationFails()
 			.multiPart("arquivo", new File("src/main/resources/arquivoGrande.zip"))
 		.when()
 			.post("http://restapi.wcaquino.me/upload")
 		.then()
-			.log().all()
+			.log().ifValidationFails()
 			.time(lessThan(2300L))
 			.statusCode(413)
 		;
 	}
 	
-	
+	@Test
+	public void deveBaxarArquivo() throws IOException {
+		byte[] image = given()
+			.log().all()
+		.when()
+			.get("http://restapi.wcaquino.me/download")
+		.then()
+			.log().ifValidationFails()
+			.statusCode(200)
+			.extract().asByteArray()
+		;
+		
+		File imagem = new File("src/main/resources/file.jpg");
+		OutputStream out = new FileOutputStream(imagem);
+		out.write(image);
+		out.close();
+		
+		MatcherAssert.assertThat(imagem.length(), lessThan(95000L));
+	}
 
 }
