6f10284f7dae73f56b70a35897bbe9f1d72094d7
Luciano Mendonca <luciano.s.mend@gmail.com>
2024-05-02 21:31:15 -0300

    Validação de schema JSON

--- src/main/java/br/luciano/rest/SchemaTest.java
+++ src/main/java/br/luciano/rest/SchemaTest.java
@@ -6,6 +6,7 @@ import org.junit.Test;
 import org.xml.sax.SAXParseException;
 
 import io.restassured.matcher.RestAssuredMatchers;
+import io.restassured.module.jsv.JsonSchemaValidator;
 
 public class SchemaTest {
 	
@@ -32,6 +33,19 @@ public class SchemaTest {
 			.log().ifValidationFails()
 			.statusCode(200)
 			.body(RestAssuredMatchers.matchesXsdInClasspath("users.xsd"))
+		;
+	}
+	
+	@Test
+	public void deveValidarSchemaJson() {
+		given()
+			.log().ifValidationFails()
+		.when()
+			.get("https://restapi.wcaquino.me/users")
+		.then()
+			.log().ifValidationFails()
+			.statusCode(200)
+			.body(JsonSchemaValidator.matchesJsonSchemaInClasspath("users.json"))
 		;
 	}
 
